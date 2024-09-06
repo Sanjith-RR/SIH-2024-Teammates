0b1e67194dcee2c0acac49b5a8a9a51b4519a759
Sanjith <rrsanjith@gmail.com>
2024-09-06 11:19:59 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import LoginPage from "@/components/login/LoginPage";
+import { GlobeDemo } from "@/components/login/ThreeGlobe";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <LoginPage/>
+    </div>
+  );
+}
