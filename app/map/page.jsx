276bd25a875df78b9944a9eda0ec88dd5ac9b86a
PIAI23MAR1010 <sshermathangam@gmail.com>
2024-09-08 13:10:32 +0530

    Deploy update

--- app/map/page.jsx
+++ app/map/page.jsx
@@ -1,29 +1,13 @@
-'use client'; // This ensures the page is rendered only on the client-side
+// page.js
+'use client';
 
-import React, { useEffect, useState } from 'react';
-import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet';
+import dynamic from 'next/dynamic';
 import { useRouter } from 'next/navigation';
 
-const WMSLayer = () => {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true); // Ensure this runs only on the client-side
-
-    if (isClient) {
-      const map = L.map('map').setView([20.5937, 78.9629], 4);
-      L.tileLayer.wms('https://bhuvan-ras2.nrsc.gov.in/cgi-bin/LULC250K.exe', {
-        layers: 'LULC250K_2223',
-        format: 'image/png',
-        transparent: true,
-      }).addTo(map);
-    }
-  }, [isClient]);
-
-  return null;
-};
+// Dynamically import MapComponent with SSR disabled
+const MapComponent = dynamic(() => import('../../components/Map/MapComponent'), {
+  ssr: false,
+});
 
 const Page = () => {
   const navigate = useRouter();
@@ -36,19 +20,8 @@ const Page = () => {
           <span className="text-blue-500">S</span>hinka <span className="text-blue-500">J</span>inzai
         </h1>
       </header>
-      <div id="map" className="h-[89vh] w-screen">
-        {isClient && (
-          <MapContainer center={[20.5937, 78.9629]} zoom={4} className="h-full w-full">
-            <TileLayer
-              attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={[13.0843, 80.2705]}>
-              <Popup>Hello Chennai</Popup>
-            </Marker>
-            <WMSLayer />
-          </MapContainer>
-        )}
+      <div className="h-[89vh] w-screen">
+        <MapComponent />
       </div>
     </main>
   );
